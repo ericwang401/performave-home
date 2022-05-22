5cb9a3839ba4fb65ebe840780b7ade8a5bde6622
Eric Wang <eric.wang401@gmail.com>
2022-05-22 21:02:39 +0200

    Add products and hero

---
+++ components/Products.tsx
@@ -0,0 +1,73 @@
+import Image from 'next/image'
+
+const posts = [
+    {
+        title: 'Boost your conversion rate',
+        href: '#',
+        category: 'Article',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+        imageUrl:
+            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    },
+    {
+        title: 'How to use search engine optimization to drive sales',
+        href: '#',
+        category: 'Video',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
+        imageUrl:
+            'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    },
+    {
+        title: 'Improve your customer experience',
+        href: '#',
+        category: 'Case Study',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+        imageUrl:
+            'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    },
+]
+
+const Products = () => {
+    return (
+        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+            <div className="absolute inset-0">
+                <div className="bg-white h-1/3 sm:h-2/3" />
+            </div>
+            <div className="relative max-w-7xl mx-auto">
+                <div className="text-center">
+                    <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Our Products</h2>
+                    <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+                        Built for the modern web, we&apos;re constantly pushing the boundaries of what&apos;s possible.
+                    </p>
+                </div>
+                <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                    {posts.map((post) => (
+                        <div key={post.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                            <div className="flex-shrink-0">
+                                <div className='relative h-48 w-full overflow-hidden'>
+                                    <Image layout='fill' className="object-cover" src={post.imageUrl} alt="" />
+                                </div>
+                            </div>
+                            <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                                <div className="flex-1">
+                                    <p className="text-sm font-medium text-blue-600">
+                                        {post.category}
+                                    </p>
+                                    <a href={post.href} className="block mt-2">
+                                        <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                                        <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Products
